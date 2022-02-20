09e1d430c35c1a841be9dcc33d2b87a3b35c1638
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2022-02-20 17:55:50 -0500

    Tabla

--- src/components/Tabla/index.tsx
+++ src/components/Tabla/index.tsx
@@ -1,2 +1,37 @@
+import { FC } from "react";
+import { connect } from "react-redux";
+import { IUser } from "../../types/IUser";
 
+type AppProps = {
+  users: [];
+};
 
+const Tabla: FC<AppProps> = ({ users }) => {
+  const putRow = () =>
+    users.map(({ id, name, email, website }: IUser) => (
+      <tr key={id}>
+        <td>{name}</td>
+        <td>{email}</td>
+        <td>{website}</td>
+      </tr>
+    ));
+
+  return (
+    <table className="tabla">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Correo</th>
+          <th>Enlace</th>
+        </tr>
+      </thead>
+      <tbody>{putRow()}</tbody>
+    </table>
+  );
+};
+
+const mapStateToProps = (reducers: any) => {
+  return reducers.usuariosReducer;
+};
+
+export default connect(mapStateToProps)(Tabla);
--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,34 +1,17 @@
-import { FC } from 'react';
-import { IUser } from './types/IUser';
-import useGetUsers from './hooks/useGetUsers';
-import Spinner from '../../components/Spinner';
-import Error from '../../components/Error';
+import { FC } from "react";
+import useGetUsers from "./hooks/useGetUsers";
+import Spinner from "../../components/Spinner";
+import Error from "../../components/Error";
+import Tabla from "../../components/Tabla";
 const Users: FC = () => {
-    const { users, loading, error } = useGetUsers();
-    return (
-        <div className="margen">
-            <Error error={error} />
-            <table className="tabla">
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Correo</th>
-                        <th>Enlace</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user: IUser) => (
-                        <tr key={user.id}>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.website}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <>{loading && <Spinner />}</>
-        </div>
-    );
+  const { users, loading, error } = useGetUsers();
+  return (
+    <div className="margen">
+      <Error error={error} />
+      <Tabla users={users} />
+      <>{loading && <Spinner />}</>
+    </div>
+  );
 };
 
 export default Users;
