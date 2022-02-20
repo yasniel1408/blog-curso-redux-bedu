19b941b8df3789916b1e3ee061ff3aac35d00476
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2022-02-20 17:48:02 -0500

    done

--- src/redux/actions/userActions.ts
+++ src/redux/actions/userActions.ts
@@ -4,7 +4,7 @@ import { ERROR_USERS, FETCH_USERS, SUCCESS_USERS } from '../types/usersTypes';
 export const getAll = () => async (dispatch: any) => {
     try {
         dispatch({ type: FETCH_USERS });
-        let res = await axios.get('https://jsonplaceholder.typicode.com/usersa');
+        let res = await axios.get('https://jsonplaceholder.typicode.com/users');
         dispatch({
             type: SUCCESS_USERS,
             payload: res.data,
